847208e80133c256469c7666fc0beb3d5f717ecb
GoncharSY <gonchar.sy.dev@gmail.com>
2023-11-13 04:15:44 +0300

    Решил третью задачу (поиск в списке адресов)

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,21 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      query: ""
+    };
+  },
+
+  computed: {
+    emails() {
+      let query = this.query.toLowerCase();
+
+      return emails.map(itm => ({
+        email: itm,
+        marked: !!query && itm.toLowerCase().includes(query)
+      }));
+    }
+  }
+}).mount('#app');
