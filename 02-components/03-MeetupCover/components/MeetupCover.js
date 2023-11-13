6f7bd8fb8ee01768c5189fa2c3203589500138cb
GoncharSY <gonchar.sy.dev@gmail.com>
2023-11-13 09:45:09 +0300

    Решил третью задачу (обложка митапа)

--- 02-components/03-MeetupCover/components/MeetupCover.js
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -3,10 +3,31 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    image: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+
+  computed: {
+    style() {
+      return {
+        '--bg-url': this.image && `url(${this.image})`,
+      };
+    },
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
+    <div class="meetup-cover" :style="style">
         <!-- Ссылка выше - пример верстки при наличии изображения, а не изображение по умолчанию. -->
         <!-- Изображение по умолчанию уже есть в стилях. Для его вывода достаточно не добавлять никаких стилей. -->
-        <h1 class="meetup-cover__title">Title</h1>
+        <h1 class="meetup-cover__title">{{title}}</h1>
     </div>`,
 });
