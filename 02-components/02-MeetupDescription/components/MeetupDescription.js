04092679f87cc620f6169afba44c68a76e45ac56
GoncharSY <gonchar.sy.dev@gmail.com>
2023-11-13 09:17:50 +0300

    Решил вторую задачу (описание митапа)

--- 02-components/02-MeetupDescription/components/MeetupDescription.js
+++ 02-components/02-MeetupDescription/components/MeetupDescription.js
@@ -3,5 +3,13 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupDescription',
 
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
+  props: {
+    description: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+
+  template: `<p class="meetup-description">{{description}}</p>`,
 });
