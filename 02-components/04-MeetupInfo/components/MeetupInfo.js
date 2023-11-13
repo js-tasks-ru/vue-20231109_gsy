afa0fcdca9b553d118ffc6684e321353db57868c
GoncharSY <gonchar.sy.dev@gmail.com>
2023-11-13 11:06:16 +0300

    Решил четвертую задачу (организатор, место и время митапа)

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,48 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    isoDate() {
+      return new Date(this.date).toISOString().slice(0, 10);
+    },
+
+    localeDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      });
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="isoDate">{{localeDate}}</time>
       </li>
     </ul>`,
 });
