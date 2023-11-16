d9642187f5389246f4f07a6948713d56f2dc1b11
GoncharSY <gonchar.sy.dev@gmail.com>
2023-11-16 14:27:14 +0300

    Решил вторую задачу (анимация списка митапов)

--- 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
+++ 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
@@ -17,30 +17,36 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* _transitions.css */
 
 .fade-list {
   position: relative;
 }
 
-.fade-list > * {
+/* .fade-list > *  */
+/* :slotted(*) Должно быть, но не работает. */
+:deep(*) {
   opacity: 1;
   transition: opacity 0.3s ease-out;
 }
 
-.fade-list .fade-list-leave-active {
+/* .fade-list .fade-list-leave-active, */
+:deep(.fade-list-leave-active) {
   position: absolute !important;
   left: 0;
   right: 0;
 }
 
-.fade-list .fade-list-enter-from,
-.fade-list .fade-list-leave-to {
+/* .fade-list .fade-list-enter-from, */
+/* .fade-list .fade-list-leave-to, */
+:deep(.fade-list-enter-from),
+:deep(.fade-list-leave-to) {
   opacity: 0;
 }
 
-.fade-list .fade-list-move {
+/* .fade-list .fade-list-move, */
+:deep(.fade-list-move) {
   transition: transform 0.3s;
 }
 </style>
