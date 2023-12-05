e34a76c9fb33c538e5d4aad71de13874f85b628d
GoncharSY <gonchar.sy.dev@gmail.com>
2023-12-06 00:34:43 +0300

    Переделал компонент для ввода даты

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,6 @@
 <template>
-  <UiInput ref="input" :type="type" :step="step" v-model="valueProxy">
+  <UiInput ref="input" :type="type" :step="step" :modelValue="stringValue" @input="onInput">
+
     <template v-for="slotName in Object.keys($slots)" #[slotName]>
       <slot :name="slotName" />
     </template>
@@ -28,33 +29,31 @@
 
       step: {
         type: [Number, String],
-        default: 1
+        default: 1,
       },
     },
 
     computed: {
-      valueProxy: {
-        get() {
-          if (!this.modelValue) return '';
-          let inp = this.makeProxyInput();
-          inp.valueAsNumber = this.modelValue;
-          return inp.value;
-        },
-        set(val) {
-          let inp = this.makeProxyInput();
-          inp.value = val;
-          this.$emit('update:modelValue', inp.valueAsNumber);
+      stringValue() {
+        if (!this.modelValue) return '';
+
+        let val = new Date(this.modelValue)
+          .toISOString()
+          .slice(0, 19)
+          .replace(/(:00)$/, '');
+
+        switch (this.type) {
+          case 'date': return val.slice(0, 10);
+          case 'time': return val.slice(11);
+          default: return val;
         }
       },
     },
 
     methods: {
-      makeProxyInput() {
-        let inp = document.createElement('input');
-        inp.type = this.type;
-        inp.step = this.step;
-        return inp;
-      }
+      onInput(evt) {
+        this.$emit('update:modelValue', evt.target.valueAsNumber);
+      },
     },
   };
 </script>
