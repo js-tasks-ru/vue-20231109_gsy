1c0a062072cf8bf10a5ec4b04a7dd5bdfc25e04e
GoncharSY <gonchar.sy.dev@gmail.com>
2023-12-18 15:29:56 +0300

    Решил 3-ю задачу (рисовка списка панелей)
    Для визуальной проверки изменил и файл 'App.vue'. Теперь в нем элементы устанавливаются в слот по умолчанию компонента 'UiPanes'.

--- 09-rendering/03-UiPanes-JSX/App.vue
+++ 09-rendering/03-UiPanes-JSX/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div class="sample container">
-    <!-- Должны выводиться 4 панели без именованных слотов <template #pane-X> -->
     <UiPanes>
-      <template #pane-0>
+      <!-- <template #pane-0>
         <div>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum harum id labore repellat sequi. Aliquam at
           consequuntur dolorem doloremque dolorum eum excepturi, incidunt laudantium, molestias quidem reprehenderit
           sunt suscipit ullam.
         </div>
-      </template>
-      <template #pane-1>
+      </template> -->
+
+      <!-- <template #pane-1>
         <UiButton variant="primary">Pane 2 - Button</UiButton>
-      </template>
-      <template #pane-2>
+      </template> -->
+
+      <!-- <template #pane-2>
         <UiInput placeholder="Pane 3 - Input" />
-      </template>
-      <template #pane-3>
+      </template> -->
+
+      <!-- <template #pane-3>
         <div>
           <UiIcon icon="search" />
           <UiIcon icon="tv" />
           <UiIcon icon="coffee" />
           <UiIcon icon="key" />
         </div>
-      </template>
+      </template> -->
+
+      <div>Content of Pane-01</div>
+      <div>Content of Pane-02</div>
+      <div>Content of Pane-03</div>
+      <div>Content of Pane-04</div>
+
+      <UiButton variant="primary">Pane 5 - Button</UiButton>
+      <UiInput placeholder="Pane 6 - Input" />
+
+      <div>
+        <UiIcon icon="search" />
+        <UiIcon icon="tv" />
+        <UiIcon icon="coffee" />
+        <UiIcon icon="key" />
+      </div>
     </UiPanes>
   </div>
 </template>
