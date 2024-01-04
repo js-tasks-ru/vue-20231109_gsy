c9f4ef951b8b5b64c4b68e865aeb7898e8578b4e
GoncharSY <gonchar.sy.dev@gmail.com>
2024-01-04 04:45:02 +0300

    Решил 1-ю задачу (прогресс-бар, плагин)

--- 13-reusability/01-ProgressPlugin/plugins/progress/index.js
+++ 13-reusability/01-ProgressPlugin/plugins/progress/index.js
@@ -1,14 +1,44 @@
 import { createApp, inject } from 'vue';
 import TheTopProgressBar from './TheTopProgressBar.vue';
 
-// Используйте эту константу в качестве ключа provide/inject
+/** Используйте эту константу в качестве ключа provide/inject. */
 export const PROGRESS_KEY = Symbol('PROGRESS_KEY');
 
-// Функция для удобного внедрения с Composition API
+/** Функция для удобного внедрения с Composition API. */
 export function useProgress() {
   return inject(PROGRESS_KEY);
 }
 
+/**
+ * Создать новый плагин с прогресс-баром.
+ * @param {Object} opt           Объект с параметрами.
+ * @param {Router} opt.router    Маршрутизатор приложения.
+ * @param {string} opt.container Контейнер для размещения прогресс-бара.
+ * @returns {Object} Плагин.
+ */
 export function createProgress({ container, router } = {}) {
-  // Решение
+  const cont = container ?? createContainer();
+  const prog = createApp(TheTopProgressBar).mount(cont);
+
+  const plug = {
+    fail: prog.fail.bind(prog),
+    start: prog.start.bind(prog),
+    finish: prog.finish.bind(prog),
+    install: (app) => app.provide(PROGRESS_KEY, plug),
+  };
+
+  if (router) {
+    router.onError(() => prog.fail());
+    router.afterEach(() => prog.finish());
+    router.beforeEach(() => prog.start());
+  }
+
+  return plug;
+}
+
+/** Создать контейнер 'div' для прогресс-бара. */
+function createContainer() {
+  const elem = document.createElement('div');
+  const place = document.body;
+  return place.appendChild(elem);
 }
